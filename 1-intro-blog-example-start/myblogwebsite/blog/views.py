999e472b9eaa4e8954d941db084b0919fc982c40
Derek <derek358@icloud.com>
2026-01-14 20:49:42 -0700

    Added to Intro

---
+++ 1-intro-blog-example-start/myblogwebsite/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# Below load the post model from the models.py file
+from .models import Post
+
+# Create your views here.
+# this is what will handle the request and retrun a response
+def post_list(request):
+    # get all the posts from the database
+    posts = Post.objects.all()
+    # breakpoint()
+    # the following will render the template we created
+    return render(request, 'blog/post_list.html', {'posts': posts})
